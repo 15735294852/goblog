242fbc3fe74998b3039ec48c3aa776aa139b4fb1
wangxiaoshuai <wangshuai@zhongwenweilai.com>
2020-03-22 17:09:30 +0800

    add

--- main.go
+++ main.go
@@ -1,48 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"time"
 )
 
 func main() {
-	p("ChitChat", version(), "started at", config.Address)
 
-	// handle static assets
-	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir(config.Static))
-	mux.Handle("/static/", http.StripPrefix("/static/", files))
 
+
+	http.HandleFunc("/zc",hello)
+
+	http.ListenAndServe(":8080",nil)
+
+
+	//p("ChitChat", version(), "started at", config.Address)
 	//
-	// all route patterns matched here
-	// route handler functions defined in other files
+	//// handle static assets
+	//mux := http.NewServeMux()
+	//files := http.FileServer(http.Dir(config.Static))
+	//mux.Handle("/static/", http.StripPrefix("/static/", files))
 	//
+	////
+	//// all route patterns matched here
+	//// route handler functions defined in other files
+	////
+	//
+	//// index
+	//mux.HandleFunc("/", index)
+	//// error
+	//mux.HandleFunc("/err", err)
+	//
+	//// defined in route_auth.go
+	//mux.HandleFunc("/login", login)
+	//mux.HandleFunc("/logout", logout)
+	//mux.HandleFunc("/signup", signup)
+	//mux.HandleFunc("/signup_account", signupAccount)
+	//mux.HandleFunc("/authenticate", authenticate)
+	//
+	//// defined in route_thread.go
+	//mux.HandleFunc("/thread/new", newThread)
+	//mux.HandleFunc("/thread/create", createThread)
+	//mux.HandleFunc("/thread/post", postThread)
+	//mux.HandleFunc("/thread/read", readThread)
+	//
+	//// starting up the server
+	//server := &http.Server{
+	//	Addr:           config.Address,
+	//	Handler:        mux,
+	//	ReadTimeout:    time.Duration(config.ReadTimeout * int64(time.Second)),
+	//	WriteTimeout:   time.Duration(config.WriteTimeout * int64(time.Second)),
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+	//server.ListenAndServe()
+}
 
-	// index
-	mux.HandleFunc("/", index)
-	// error
-	mux.HandleFunc("/err", err)
+func hello(w http.ResponseWriter, r *http.Request) {
 
-	// defined in route_auth.go
-	mux.HandleFunc("/login", login)
-	mux.HandleFunc("/logout", logout)
-	mux.HandleFunc("/signup", signup)
-	mux.HandleFunc("/signup_account", signupAccount)
-	mux.HandleFunc("/authenticate", authenticate)
+	fmt.Fprintf(w,"Hello Docker Form Golang!")
 
-	// defined in route_thread.go
-	mux.HandleFunc("/thread/new", newThread)
-	mux.HandleFunc("/thread/create", createThread)
-	mux.HandleFunc("/thread/post", postThread)
-	mux.HandleFunc("/thread/read", readThread)
-
-	// starting up the server
-	server := &http.Server{
-		Addr:           config.Address,
-		Handler:        mux,
-		ReadTimeout:    time.Duration(config.ReadTimeout * int64(time.Second)),
-		WriteTimeout:   time.Duration(config.WriteTimeout * int64(time.Second)),
-		MaxHeaderBytes: 1 << 20,
-	}
-	server.ListenAndServe()
 }
